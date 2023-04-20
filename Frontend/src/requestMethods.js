854d2f124e888d56ab344e0b9b629dbea695fa2f
unknown <jameskagunga2022@gmail.com>
2023-04-20 03:26:16 -0700

    added some changes

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = "http://64.226.67.231:5000/api/";
+const BASE_URL = "https://api.afrikanaccentadventures.com/api/";
 
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGI2NzM2ZjJmZDM2YTBlMjA5YWE3MSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDQ5MTQ1MDUsImV4cCI6MTY0NTE3MzcwNX0.TRfdqYrZXhFrj7kYmpv1jnQtYMxMPu-unP-g9cYux4Q";
 
